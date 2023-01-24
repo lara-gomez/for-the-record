cbd91ebe4ae1a03867f55a04f68644e725f4b83b
Lara Gomez <laragomez@Laras-MBP-2.attlocal.net>
2023-01-24 03:12:08 -0500

    added spotify api/auth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,7 @@ import jwt_decode from "jwt-decode";
 import NotFound from "./pages/NotFound.js";
 import Skeleton from "./pages/Skeleton.js";
 import NavBar from "./modules/Navbar.js";
+import Spotify from "./pages/Spotify.js";
 
 import "../utilities.css";
 
@@ -47,6 +48,7 @@ const App = () => {
       <NavBar />
       <Router>
         <Skeleton path="/" handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} />
+        <Spotify path="/spotifyPage" />
         <NotFound default />
       </Router>
     </>
--- server/api.js
+++ server/api.js
@@ -39,6 +39,84 @@ router.post("/initsocket", (req, res) => {
   res.send({});
 });
 
+// Spotify API methods
+
+let access_token = ""; // initiate access token
+const spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
+const spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const callback_uri = "http://localhost:5050/api/spotify/callback";
+/**
+ * Generates a secure random token (https://stackoverflow.com/questions/8855687/secure-random-token-in-node-js)
+ */
+function generateRandomString() {
+  require("crypto").randomBytes(48, function (err, buffer) {
+    return buffer.toString("hex");
+  });
+}
+
+/**
+ * API endpoint for pulling up the spotify login page. If the page doesn't pull up its probably because your browser remembers ur login session.
+ * If you need to see the Spotify login page for testing purposes use an incognito window, it will forget all your browser data.
+ */
+router.get("/spotify/login", (req, res) => {
+  var scope =
+    "streaming \
+                 user-read-email \
+                 user-read-private";
+
+  const state = generateRandomString(16);
+
+  var auth_query_parameters = new URLSearchParams({
+    response_type: "code",
+    client_id: spotify_client_id,
+    scope: scope,
+    redirect_uri: callback_uri, // make sure that this is your FRONTEND! localhost:5050 points to the front end not backend for me
+    state: state,
+  });
+
+  res.redirect("https://accounts.spotify.com/authorize/?" + auth_query_parameters.toString());
+});
+
+/**
+ * This is the callback function that runs after a user logs in through the spotify login page. It will fetch an access token from the Spotify servers.
+ */
+router.get("/spotify/callback", (req, res) => {
+  var code = req.query.code;
+
+  var authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    form: {
+      code: code,
+      redirect_uri: callback_uri,
+      grant_type: "authorization_code",
+    },
+    headers: {
+      Authorization:
+        "Basic " + Buffer.from(spotify_client_id + ":" + spotify_client_secret).toString("base64"),
+      "Content-Type": "routerlication/x-www-form-urlencoded",
+    },
+    json: true,
+  };
+
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      access_token = body.access_token; // this is the access token that you will use to send to the Spotify API anytime you send them an API call.
+      res.redirect("/"); // sends user back to the home page after a token is fetched. Up to you to change this to whatever path you want.
+    }
+  });
+});
+
+/**
+ * Called as soon as someone opens up localhost:5050/spotifyPage in order to check if there is an access token available on the server.
+ * You can use this to render different things and check if someone has logged in on the front end.
+ */
+router.get("/spotify/token", (req, res) => {
+  console.log("token page");
+  res.json({
+    access_token: access_token,
+  });
+});
+
 // |------------------------------|
 // | write your API methods below!|
 // |------------------------------|
